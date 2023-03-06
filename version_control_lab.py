50833056ff4f445eaa63016d98c7651b6a8e0c8f
hachoj <chojnowski.harrison@gmail.com>
2023-03-05 23:08:57 -0500

    first commit, added a the main file with a little bit of code

---
+++ version_control_lab.py
@@ -0,0 +1,19 @@
+def encoder(data):
+    data_list = []
+    data_list = data[:0]
+    return_data_list = []
+    for num in data_list:
+        num += 3
+        if num > 9:
+            num -= 10
+        return_data_list.append(num)
+    return ''.join(return_data_list)
+
+
+
+def main()
+    pass
+
+
+if__name__ == '__main__':
+    main()
