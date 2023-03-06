52c574cd2655161bf5fed980a8f0e4824ed1809d
hachoj <chojnowski.harrison@gmail.com>
2023-03-05 23:32:40 -0500

    Finished personal portion of lab

--- version_control_lab.py
+++ version_control_lab.py
@@ -1,3 +1,4 @@
+# Harrison Chojnowski's encoder function
 def encoder(data):
     data_list = list(data)
     return_data_list = []
@@ -11,8 +12,23 @@ def encoder(data):
 
 
 def main():
-    pass
-
+    playing = True
+    while playing:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+        choice = input('Please enter an option: ')
+        encoded_password = ''
+        if choice == '1':
+            user_input = input('Please enter your password to encode: ')
+            encoded_password = encoder(user_input)
+            print('Your password has been encoded and stored!\n')
+        elif choice == '2':
+            print(f'The encoded passwrod is {encoded_password}, and the original password is {decoder(encoded_password)}.\n')
+        elif choice == '3':
+            playing = False
 
 if __name__ == '__main__':
     main()
