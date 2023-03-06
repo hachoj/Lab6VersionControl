41affb40d62462492d2e14fa4c1b63969c7ded63
hachoj <chojnowski.harrison@gmail.com>
2023-03-05 23:21:05 -0500

    forgot to add main file

--- version_control_lab.py
+++ version_control_lab.py
@@ -1,12 +1,12 @@
 def encoder(data):
-    data_list = []
-    data_list = data[:0]
+    data_list = list(data)
     return_data_list = []
     for num in data_list:
+        num = int(num)
         num += 3
         if num > 9:
             num -= 10
-        return_data_list.append(num)
+        return_data_list.append(str(num))
     return ''.join(return_data_list)
 
 
