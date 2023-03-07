7b7098a34790048b4755a4775e544b145dac9dd3
hachoj <chojnowski.harrison@gmail.com>
2023-03-06 23:09:11 -0500

    minor changes

--- test_version_control_lab.py
+++ test_version_control_lab.py
@@ -5,3 +5,4 @@ from version_control_lab import *
 class Test(TestCase):
     def test_encoder(self):
         self.assertEqual('45678888', encoder('12345555'))
+        self.assertEqual('345628', encoder('012395'))
--- version_control_lab.py
+++ version_control_lab.py
@@ -1,14 +1,13 @@
 # Harrison Chojnowski's encoder function
 def encoder(data):
-    data_list = list(data)
-    return_data_list = []
-    for num in data_list:
+    encoded_data = ''
+    for num in data:
         num = int(num)
         num += 3
         if num > 9:
             num -= 10
-        return_data_list.append(str(num))
-    return ''.join(return_data_list)
+        encoded_data += str(num)
+    return encoded_data
 
 
 def main():
